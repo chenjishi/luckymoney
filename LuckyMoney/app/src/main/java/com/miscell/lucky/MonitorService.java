f03e479bd58c9bafac348d994c4bddab59396606
jishi chen <chenjishi313@gmail.com>
2017-04-06 17:42:44 +0800

    improved stability

--- LuckyMoney/app/src/main/java/com/miscell/lucky/MonitorService.java
+++ LuckyMoney/app/src/main/java/com/miscell/lucky/MonitorService.java
@@ -3,6 +3,8 @@ package com.miscell.lucky;
 import android.accessibilityservice.AccessibilityService;
 import android.app.KeyguardManager;
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.os.PowerManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
@@ -15,17 +17,36 @@ import static android.view.accessibility.AccessibilityNodeInfo.ACTION_CLICK;
 /**
  * Created by chenjishi on 15/2/12.
  */
-// TODO: 2017/1/24 remember add logic do not depend on notification notify to open lucky
 public class MonitorService extends AccessibilityService {
-    private boolean mLuckyClicked;
+    private static final int MSG_BACK = 233;
+
+    private static final String UI_RECEIVE = "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI";
+    private static final String UI_DETAIL = "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI";
+
+    private boolean luckyClicked, hasLucky;
+
+    private final Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == MSG_BACK) {
+                performGlobalAction(GLOBAL_ACTION_BACK);
+                postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        performGlobalAction(GLOBAL_ACTION_BACK);
+                    }
+                }, 1000);
+            }
+        }
+    };
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         final int eventType = event.getEventType();
 
         if (eventType == TYPE_NOTIFICATION_STATE_CHANGED) {
-            mLuckyClicked = false;
-            unlockScreen();
+            if (isLockScreen()) unlockScreen();
+            luckyClicked = false;
         }
 
         if (eventType == TYPE_WINDOW_CONTENT_CHANGED) {
@@ -35,42 +56,39 @@ public class MonitorService extends AccessibilityService {
             List<AccessibilityNodeInfo> list = rootNode.findAccessibilityNodeInfosByText(getString(R.string.get_lucky));
             if (null == list || list.size() == 0) return;
 
-            AccessibilityNodeInfo node = list.get(list.size() - 1);
-
-            if (node.isClickable()) {
-                node.performAction(ACTION_CLICK);
-            } else {
-                AccessibilityNodeInfo parentNode = node;
-                for (int i = 0; i < 5; i++) {
-                    if (null != parentNode) {
-                        parentNode = parentNode.getParent();
-                        if (null != parentNode && parentNode.isClickable() && !mLuckyClicked) {
-                            mLuckyClicked = true;
-                            parentNode.performAction(ACTION_CLICK);
-                        }
-                    }
+            AccessibilityNodeInfo parent = list.get(list.size() - 1);
+            while (null != parent) {
+                if (parent.isClickable() && !luckyClicked) {
+                    parent.performAction(ACTION_CLICK);
+                    luckyClicked = true;
+                    break;
                 }
+                parent = parent.getParent();
             }
         }
 
         if (eventType == TYPE_WINDOW_STATE_CHANGED) {
             String clazzName = event.getClassName().toString();
-            if (clazzName.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI")) {
-                AccessibilityNodeInfo nodeInfo = event.getSource();
-                traverseNode(nodeInfo);
+            if (clazzName.equals(UI_RECEIVE)) {
+                traverseNode(event.getSource());
+            }
+
+            if (clazzName.equals(UI_DETAIL) && hasLucky) {
+                hasLucky = false;
+                handler.sendEmptyMessageDelayed(MSG_BACK, 1000);
             }
         }
     }
 
     private void traverseNode(AccessibilityNodeInfo node) {
         if (null == node) return;
-
         final int count = node.getChildCount();
         if (count > 0) {
             for (int i = 0; i < count; i++) {
                 AccessibilityNodeInfo childNode = node.getChild(i);
                 if (null != childNode && childNode.getClassName().equals("android.widget.Button") && childNode.isClickable()) {
                     childNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                    hasLucky = true;
                 }
 
                 traverseNode(childNode);
@@ -79,16 +97,20 @@ public class MonitorService extends AccessibilityService {
     }
 
     private void unlockScreen() {
-        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
-        final KeyguardManager.KeyguardLock keyguardLock = keyguardManager.newKeyguardLock("MyKeyguardLock");
-        keyguardLock.disableKeyguard();
-
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-        PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK
-                | PowerManager.ACQUIRE_CAUSES_WAKEUP
-                | PowerManager.ON_AFTER_RELEASE, "MyWakeLock");
+        PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP
+                | PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "light_screen");
+        wakeLock.acquire(1000);
+        wakeLock.release();
 
-        wakeLock.acquire();
+        KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        KeyguardManager.KeyguardLock keyguardLock = km.newKeyguardLock("unlock_screen");
+        keyguardLock.disableKeyguard();
+    }
+
+    private boolean isLockScreen() {
+        KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        return km.inKeyguardRestrictedInputMode();
     }
 
     @Override
